67e1147690288b6e22b7d1db9d74589e13bd703e
YangSuGuo <3130876713@qq.com>
2024-04-07 15:36:07 +0800

    admin项目初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes: Array<RouteRecordRaw> = [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+        path: '/',
+        name: 'home',
+        component: HomeView
     }
-  ]
+]
+
+const router =  createRouter({
+  history: createWebHistory(),
+  routes
 })
 
 export default router
